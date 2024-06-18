7e9f6a056322f84dd3d73d352f4a03890cd72504
Asier <axigonzalez2012@gmail.com>
2024-06-18 12:19:51 +0200

    @Qualifier

--- prueba/src/main/java/com/example/ioc/SaludaImpl2.java
+++ prueba/src/main/java/com/example/ioc/SaludaImpl2.java
@@ -1,8 +1,10 @@
 package com.example.ioc;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component("saludaEn")
+@Qualifier("en")
 public class SaludaImpl2 implements Saluda {
 	
 	Entorno entorno;
