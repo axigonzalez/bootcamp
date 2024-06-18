21fd3130782ce13d588d567a9584731e9771deec
Asier <axigonzalez2012@gmail.com>
2024-06-18 13:57:55 +0200

    Optional.ofNullable

--- prueba/src/main/java/com/example/ioc/SaludaImpl2.java
+++ prueba/src/main/java/com/example/ioc/SaludaImpl2.java
@@ -1,5 +1,7 @@
 package com.example.ioc;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
@@ -24,5 +26,9 @@ public class SaludaImpl2 implements Saluda {
 	@Override
 	public int getContador() {
 		return entorno.getContador();
+	}
+	
+	public Optional <Entorno>getEntorno(){
+		return Optional.ofNullable(entorno);
 	}
 }
