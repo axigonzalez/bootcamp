e390f29740b0902ae6d668a3fbb0a333529ef50c
Asier <axigonzalez2012@gmail.com>
2024-06-24 13:21:14 +0200

    crud

--- prueba/src/main/java/com/example/PruebaApplication.java
+++ prueba/src/main/java/com/example/PruebaApplication.java
@@ -1,14 +1,21 @@
 package com.example;
 
+import java.awt.event.ItemEvent;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.domains.contracts.repositories.ActorRepository;
+import com.example.domains.entities.Actor;
 import com.example.ioc.Rango;
 import com.example.ioc.Saluda;
 import com.example.ioc.SaludaImpl2;
+
+import ch.qos.logback.core.joran.conditional.ElseAction;
+import lombok.experimental.var;
 
 
 @SpringBootApplication
@@ -19,32 +26,49 @@ public class PruebaApplication implements CommandLineRunner {
 	}
 	
 	@Autowired
-	@Qualifier("es")
-	Saluda saluda;
-	
-	@Autowired
-	@Qualifier("en")
-	Saluda saluda2;
-	
-	@Autowired
-	Rango rango;
-	
-//	@Autowired(required = false)
-//	SaludaImpl2 kkImpl2;
-	
+	ActorRepository dao;
 	
 	@Override
 	public void run(String... args) throws Exception {
-		System.err.println("Aplicación arrancada");
-		//var saluda = new Saluda();
-		System.out.println(saluda.getContador());
-		//saluda.saluda(null);
-		saluda.saluda("mundo");
-		saluda2.saluda("world");
-		System.out.println(saluda.getContador());
-		System.out.println(saluda2.getContador());
-		System.out.println(rango.getMin() + " -> " + rango.getMax());
+//		var item = dao.findById(301);
+//		if(item.isEmpty())
+//			System.err.println("No encontrado");
+//	} else {
+//		System.out.println(item.get());
+//		
+//		dao.findAll().forEach(System.out::println);	
+//		var actor = new Actor(0, "Pepito", "Grillo");
+//		System.out.println(dao.save(actor));
+//		
+		var item = dao.findById(201);
+		if(item.isEmpty())
+			System.err.println("No encontrado");
 		
-	}
+		if(!item.isEmpty()) {
+			var actor= item.get();
+			actor.setFirstName(actor.getFirstName().toUpperCase());
+			dao.save(actor);
+		}
+//			System.out.println(item.get());
+		
+//		dao.findAll().forEach(System.out::println);	
+		}
+	
+//	@Autowired
+//	@Qualifier("es")
+//	Saluda saluda;
+//	
+//	@Autowired
+//	@Qualifier("en")
+//	Saluda saluda2;
+//	
+//	@Autowired
+//	Rango rango;
+//	
+////	@Autowired(required = false)
+////	SaludaImpl2 kkImpl2;
+//	
+	
+
 
 }
