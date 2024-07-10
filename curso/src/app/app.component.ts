8ebeda6af5dea287225c5d69c589789f74876f9d
Asier <axigonzalez2012@gmail.com>
2024-07-10 14:37:56 +0200

    notifications ampliacion

--- curso/src/app/app.component.ts
+++ curso/src/app/app.component.ts
@@ -2,11 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SecurityModule } from './security';
 import { LoggerService, MyCoreModule } from '@my/core';
+import { NotificationModalComponent } from './main';
+import { DemosComponent } from './demos/demos.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SecurityModule, MyCoreModule],
+  imports: [RouterOutlet, CommonModule, SecurityModule, MyCoreModule, NotificationModalComponent, DemosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
