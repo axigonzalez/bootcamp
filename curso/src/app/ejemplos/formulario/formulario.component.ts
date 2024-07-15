2865c77d5eeb945b1e995d2e7bacc9181f585818
Asier <axigonzalez2012@gmail.com>
2024-07-15 12:10:35 +0200

    Form

--- curso/src/app/ejemplos/formulario/formulario.component.ts
+++ curso/src/app/ejemplos/formulario/formulario.component.ts
@@ -1,17 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { ErrorMessagePipe, NIFNIEValidator, TypeValidator, UppercaseValidator } from '@my/core';
 
 @Component({
   selector: 'app-formulario',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, ErrorMessagePipe, NIFNIEValidator, UppercaseValidator, TypeValidator, ],
   templateUrl: './formulario.component.html',
   styleUrl: './formulario.component.css'
 })
 export class FormularioComponent {
   modo: 'add' | 'edit' = 'add'
-  elemento: any = { id: 0, nombre: 'Pepito', apellidos: 'Grillo', correo: 'pgrillo@example.com', edad: 99, fecha: '2024-07-15', conflictivo: true }
+  elemento: any = { }
 
   add() {
     this.elemento = {}
@@ -27,7 +28,7 @@ export class FormularioComponent {
   }
 
   send() {
-    switch (this.modo) {
+    switch(this.modo) {
       case 'add':
         window.alert(`POST: ${JSON.stringify(this.elemento)}`)
         this.cancel()
