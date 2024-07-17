bb55e2884f0dbdd467c72e4828d7ad2900706b7f
Asier <axigonzalez2012@gmail.com>
2024-07-17 11:51:07 +0200

    fixed

--- catalogoFront/src/environments/environment.development.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-    production: false,
-    ERROR_LEVEL: 3,
-    apiURL: 'http://localhost:8080/api/',
-    securityApiURL: 'http://localhost:8080/',
-  };
-  
--- catalogoFront/src/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-    production: false,
-    ERROR_LEVEL: 3,
-    apiURL: '/api/catalogo/',
-    securityApiURL: '/auth/',
-  };
-  
